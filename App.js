a6b20b82fd441b10e7cf6cf466b512799afef035
David Jungst <djungst@gmail.com>
2017-05-03 08:59:14 -0600

    fin

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import { StyleSheet } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { 
+  Container, 
+  Header, 
+  Title, 
+  Content, 
+  Button, 
+  Left, 
+  Right, 
+  Body, 
+  Icon, 
+  Text, 
+  Drawer 
+} from 'native-base';
+import { View } from 'react-native';
 import Expo from 'expo';
+import { NativeRouter, Route, Switch } from 'react-router-native';
+import Yahtzee from './Yahtzee';
+import Login from './Login';
+import Register from './Register';
+import Rules from './Rules';
+import Scores from './Scores';
+import About from './About';
+import SideBar from './SideBar';
+import ProtectedRoute from './ProtectedRoute';
 
 class App extends Component {
+  state = { drawerOpen: false }
+
   async componentWillMount() {
     await Expo.Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
@@ -11,30 +37,62 @@ class App extends Component {
     });
   }
 
+  toggleDrawer = () => {
+    this.setState({ drawerOpen: !this.state.drawerOpen }, () => {
+      if (this.state.drawerOpen)
+        this.openDrawer(); 
+      else
+        this.closeDrawer();
+    });
+  }
+
+  closeDrawer = () => {
+    this.drawer._root.close()
+  };
+
+  openDrawer = () => {
+    this.drawer._root.open()
+  };
+
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Yathzee</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>DPL - Code On!</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+      <Provider store={store}>
+        <NativeRouter>
+          <Container>
+            <Header>
+              <Left>
+                <Button transparent onPress={() => this.toggleDrawer()}>
+                  <Icon name='menu' />
+                </Button>
+              </Left>
+              <Body>
+                <Title>Yathzee</Title>
+              </Body>
+              <Right />
+            </Header>
+            <Content padder>
+              <Drawer
+                ref={ ref => { this.drawer = ref }}
+                content={<SideBar close={() => this.toggleDrawer()} navigator={this._navigator} />}
+                onClose={() => this.closeDrawer()}
+              >
+              </Drawer>
+              { this.state.drawerOpen ? null :
+                <View>
+                  <Switch>
+                    <ProtectedRoute exact path="/" component={Yahtzee} />
+                    <ProtectedRoute exact path="/scores" component={Scores} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/rules" component={Rules} />
+                  </Switch>
+                </View>
+              }
+            </Content>
+          </Container>
+        </NativeRouter>
+      </Provider>
     );
   }
 }
